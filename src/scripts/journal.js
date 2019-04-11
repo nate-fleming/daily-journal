678a7a7a7bef919450e43cd5ed7bbd16d1883ed7
nate-fleming <nfleming4@gmail.com>
2019-04-11 14:35:47 -0500

    add javaScript obects

---
+++ src/scripts/journal.js
@@ -0,0 +1,31 @@
+
+const journalEntries = []
+
+const objectJournalEntry = {
+    date: '',
+    title: '',
+    content: '',
+    mood: ''
+}
+
+const loopsJournalEntry = {
+    date: '',
+    title: '',
+    content: '',
+    mood: ''
+}
+
+const githubJournalEntry = {
+    date: '',
+    title: '',
+    content: '',
+    mood: ''
+}
+
+
+
+const addEntry = () => journalEntries.push(objectJournalEntry,loopsJournalEntry,githubJournalEntry);
+
+addEntry();
+
+console.log(journalEntries);
