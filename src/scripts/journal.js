64cbf2cf006c8ec7c696f45dba8817b439fd0515
nate-fleming <nfleming4@gmail.com>
2019-04-16 20:04:31 -0500

    change to append method

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,8 +1,12 @@
 const entryList = document.querySelector('.entryLog')
 
-
-
 const journalEntries = [
+    {
+        date: '07/24/2018',
+        title: 'Array methods',
+        content: 'We learned about 4 different array methods today. forEach made sense, but the others still confuse me.',
+        mood: 'ok'
+    },
     {
         date: '07/24/2018',
         title: 'Array methods',
